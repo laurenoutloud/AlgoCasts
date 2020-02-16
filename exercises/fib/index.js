0aea97bca70c89be446f61e82e52eb6c34601740
Lauren <lauren.finley1@yahoo.com>
2020-02-15 19:47:59 -0600

    review queues

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -7,7 +7,43 @@
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
+/*
+function fib(n) {
+    if (n <= 1){
+        return n;
+    }
+    let num;
+    let fibArr = [0,1];
 
-function fib(n) {}
+    for (let i = 2; i <= n; i++){
+        num = fibArr[0]+fibArr[1];
+        fibArr.shift();
+        fibArr.push(num);
+    }
+    return num;
+}
+*/
+/*
+function memoize(fn){
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+        const result = fn.apply(this, args);
+        cache[args] = result;
+        return result;
+    };
+}
+
+function slowFib(n){
+    if ( n <= 1){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+}
+
+const fib = memoize(slowFib);
+*/
 
 module.exports = fib;
